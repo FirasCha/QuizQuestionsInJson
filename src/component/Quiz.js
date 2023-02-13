bd50d8d5a677c97e5c982b5bb64291a052b11263
FirasCha <cri.firas@gmail.com>
2023-02-13 19:28:32 +0100

    previous and next of quiz and adding all the questions

--- src/component/Quiz.js
+++ src/component/Quiz.js
@@ -1,26 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./quiz.css"
-
+import questions from './QuizData'
 const Quiz = () => {
+    const [ currentQuestion, setCurrentQuestion] = useState(0)
+    const handleNextOptions = ()=>{
+        const nextQuestion = currentQuestion+1
+        setCurrentQuestion(nextQuestion)
+    }
+    const handlePrevOptions = ()=>{
+        const prevQuestion = currentQuestion-1
+        setCurrentQuestion(prevQuestion)
+    }
   return (
     <>
         <div className='app'>
             <div className='question-section'>
                 <h5> Score</h5>
                 <div className='question-count'>
-                    <span>Question 1 of 4</span>
+                    <span>Question {currentQuestion+1} of {questions.length}</span>
                 </div>
                 <div className='question-text'>
-                    Question 1
+                    {questions[currentQuestion].questionText}
                 </div>
             </div>
             <div className='answer-section'>
-                <button>Answers</button>
+                {questions[currentQuestion].answerOption.map((ans, i) => {
+                    return <button key={i}>{ans.answerText}</button>
+                })}
+                <div className='actions'>
+                    <button>Quit</button>
+                    {/* <button onClick={handlePrevOptions}>Previous</button> */}
+                    <button onClick={handleNextOptions}>Next</button>
+                </div>
             </div>
-            <div className='actions'>
-                <button>Quit</button>
-                <button>Next</button>
-            </div>
+            
         </div>
     </>
   )
--- src/component/QuizResult.js
+++ src/component/QuizResult.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const QuizResult = () => {
   return (
-    <div>QuizResult</div>
+    <div className='score-question'>
+        <h2>Completed!</h2>
+        <h4>Total Score 10/20</h4>
+        <h4>Your Correct Question 2 out of 4.</h4>
+        <button>Play Again</button>
+    </div>
   )
 }
 
