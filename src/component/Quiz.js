b192c967528695a0e64ba55db46654486e36f10e
FirasCha <cri.firas@gmail.com>
2023-02-13 20:35:59 +0100

    formal quiz finished but still will be bigger

--- src/component/Quiz.js
+++ src/component/Quiz.js
@@ -1,39 +1,81 @@
 import React, { useState } from 'react'
 import "./quiz.css"
 import questions from './QuizData'
+import QuizResult from './QuizResult'
 const Quiz = () => {
     const [ currentQuestion, setCurrentQuestion] = useState(0)
+    const [ score, setScore] = useState(0)
+    const [ CorrectAns, setCorrectAns] = useState(0)
+    const [ showResult, setShowResult] = useState(false)
+    const [ clicked, setClicked] = useState(false)
     const handleNextOptions = ()=>{
+        setClicked(false)
         const nextQuestion = currentQuestion+1
-        setCurrentQuestion(nextQuestion)
+        if(nextQuestion < questions.length){
+            setCurrentQuestion(nextQuestion)
+        }else{
+            setShowResult(true)
+        }    
+    }
+    const handleAnswerOption = (isCorrect)=>{
+        if(isCorrect){
+            setScore(score+5)
+            setCorrectAns(CorrectAns+1)
+        }
+        setClicked(true)
     }
     const handlePrevOptions = ()=>{
         const prevQuestion = currentQuestion-1
         setCurrentQuestion(prevQuestion)
     }
+    const handlePlayAgain= ()=>{
+        setCurrentQuestion(0)
+        setScore(0)
+        setCorrectAns(0)
+        setShowResult(false)
+    }
   return (
     <>
         <div className='app'>
-            <div className='question-section'>
-                <h5> Score</h5>
-                <div className='question-count'>
-                    <span>Question {currentQuestion+1} of {questions.length}</span>
-                </div>
-                <div className='question-text'>
-                    {questions[currentQuestion].questionText}
-                </div>
-            </div>
-            <div className='answer-section'>
-                {questions[currentQuestion].answerOption.map((ans, i) => {
-                    return <button key={i}>{ans.answerText}</button>
-                })}
-                <div className='actions'>
-                    <button>Quit</button>
-                    {/* <button onClick={handlePrevOptions}>Previous</button> */}
-                    <button onClick={handleNextOptions}>Next</button>
-                </div>
-            </div>
-            
+            {showResult?(
+
+                <QuizResult 
+                    score={score} 
+                    CorrectAns={CorrectAns} 
+                    handlePlayAgain={handlePlayAgain}
+                />
+
+            ) : (
+                <>
+                <div className='question-section'>
+                        <h5> Score : {score}</h5>
+                        <div className='question-count'>
+                            <span>Question {currentQuestion+1} of {questions.length}</span>
+                        </div>
+                        <div className='question-text'>
+                            {questions[currentQuestion].questionText}
+                        </div>
+                    </div>
+                    <div className='answer-section'>
+                        {questions[currentQuestion].answerOption.map((ans, i) => {
+                            return (
+                            <button className={`button ${clicked & ans.isCorrect? "correct":"button"}`}
+                            disabled={clicked}
+                            key={i} 
+                            onClick={()=>handleAnswerOption(ans.isCorrect)}
+                            >
+                                {ans.answerText}
+                            </button>
+                            )
+                        })}
+                        <div className='actions'>
+                            <button onClick={handlePlayAgain}>Quit</button>
+                            {/* <button onClick={handlePrevOptions}>Previous</button> */}
+                            <button disabled={!clicked} onClick={handleNextOptions}>Next</button>
+                        </div>
+                    </div>
+                </>
+                )}    
         </div>
     </>
   )
