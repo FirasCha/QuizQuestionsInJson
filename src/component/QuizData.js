b2b36c8efe130defe52959f7ee3d3a1af5ef3b21
FirasCha <cri.firas@gmail.com>
2023-02-13 18:08:58 +0100

    quiz data 1st question

---
+++ src/component/QuizData.js
@@ -0,0 +1,14 @@
+const questions = [
+    {
+        questionText : "How many days are they in the week?",
+        answerOption : [
+            {answerText : "2", isCorrect : false},
+            {answerText : "9", isCorrect : false},
+            {answerText : "7", isCorrect : true},
+            {answerText : "5", isCorrect : false},
+        ],
+    },
+    {
+
+    }
+]
