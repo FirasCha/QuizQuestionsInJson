8b002e10e33cc2905672dbbcf7fc4359f62c460e
FirasCha <cri.firas@gmail.com>
2023-02-13 18:59:34 +0100

    quiz and quizResult js

--- src/component/Quiz.js
+++ src/component/Quiz.js
@@ -5,8 +5,22 @@ const Quiz = () => {
   return (
     <>
         <div className='app'>
-            <div className='question-section'></div>
-            <div className='answer-section'></div>
+            <div className='question-section'>
+                <h5> Score</h5>
+                <div className='question-count'>
+                    <span>Question 1 of 4</span>
+                </div>
+                <div className='question-text'>
+                    Question 1
+                </div>
+            </div>
+            <div className='answer-section'>
+                <button>Answers</button>
+            </div>
+            <div className='actions'>
+                <button>Quit</button>
+                <button>Next</button>
+            </div>
         </div>
     </>
   )
