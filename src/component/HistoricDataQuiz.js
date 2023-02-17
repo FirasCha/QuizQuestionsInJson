1af62e3396291b05bc7d8e640c5752a197053849
FirasCha <cri.firas@gmail.com>
2023-02-17 18:17:07 +0100

    add more questions

--- src/component/HistoricDataQuiz.js
+++ src/component/HistoricDataQuiz.js
@@ -26,10 +26,46 @@ const historical_questions = [
             {answerText : "1953", isCorrect : false},
         ],
     },
+    {
+        questionText : "Who explored New World? ",
+        answerOption : [
+            {answerText : "Christopher Columbus", isCorrect : true},
+            {answerText : "1960", isCorrect : false},
+            {answerText : "1973", isCorrect : false},
+            {answerText : "1953", isCorrect : false},
+        ],
+    },
+    {
+        questionText : "Julius Caesar is born in which city? ",
+        answerOption : [
+            {answerText : "1963", isCorrect : false},
+            {answerText : "1960", isCorrect : false},
+            {answerText : "1973", isCorrect : false},
+            {answerText : "Rome", isCorrect : true},
+        ],
+    },
+    {
+        questionText : "In which ocean did the Titanic sink? ",
+        answerOption : [
+            {answerText : "1963", isCorrect : false},
+            {answerText : "1960", isCorrect : false},
+            {answerText : "Atlantic Ocean", isCorrect : true},
+            {answerText : "Rome", isCorrect : false},
+        ],
+    },
+    {
+        questionText : "When was the Berlin wall removed? ",
+        answerOption : [
+            {answerText : "1963", isCorrect : false},
+            {answerText : "1989", isCorrect : true},
+            {answerText : "1973", isCorrect : false},
+            {answerText : "1988", isCorrect : false},
+        ],
+    },
+    
     
     //Where is the first Summer Olympics held? Athens, Greece 1896
-    //Who explored New World? Christopher Columbus.
-    //Julius Caesar is born in which city? Rome
+
 ];
 
 export default historical_questions;
