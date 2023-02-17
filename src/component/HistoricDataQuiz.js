d4d7e8adc1e12631a84a0aa0a23c3e3112f87d5b
FirasCha <cri.firas@gmail.com>
2023-02-17 15:43:03 +0100

    create historical quiz interface

--- src/component/HistoricDataQuiz.js
+++ src/component/HistoricDataQuiz.js
@@ -8,6 +8,15 @@ const historical_questions = [
             {answerText : "1911", isCorrect : false},
         ],
     },
+    {
+        questionText : "Who is the first president of the United State? ",
+        answerOption : [
+            {answerText : "1900", isCorrect : false},
+            {answerText : "Washington", isCorrect : true},
+            {answerText : "1910", isCorrect : false},
+            {answerText : "1911", isCorrect : false},
+        ],
+    },
     
 ];
 
---
+++ src/component/HistoricalQuiz.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HistoricalQuiz = () => {
+  return (
+    <div>HistoricalQuiz</div>
+  )
+}
+
+export default HistoricalQuiz
