8967675887fd89cffdbdff0610c81ce0b2e0de18
FirasCha <cri.firas@gmail.com>
2023-02-17 20:42:54 +0100

    add routes of football

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import FootballQuiz from './component/FootballQuiz';
 import HistoricalQuiz from './component/HistoricalQuiz';
 import MenuOfQuiz from './component/MenuOfQuiz';
 import Quiz from './component/Quiz';
@@ -14,6 +15,7 @@ function App() {
       <Route path='/' element={<MenuOfQuiz/>}></Route>
         <Route path='/Quiz' element={<Quiz/>}></Route>
         <Route path='/HistoricalQuiz' element={<HistoricalQuiz/>}></Route>
+        <Route path='/FootballQuiz' element={<FootballQuiz/>}></Route>
       </Routes>
     </BrowserRouter>
     </div>
