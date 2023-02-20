619c25b64a904b5d09680671422adb31af450ca8
FirasCha <cri.firas@gmail.com>
2023-02-20 17:31:20 +0100

    add 1st two  questions

--- src/component/GeographyDataQuiz.js
+++ src/component/GeographyDataQuiz.js
@@ -1,14 +1,122 @@
-const football_questions = [
+const geography_questions = [
     {
-        questionText : "Which country won the first ever World Cup in 1930? ",
+        questionText : "What is the name of the tallest mountain in the world? ",
         answerOption : [
             {answerText : "France", isCorrect : false},
-            {answerText : "Uruguay", isCorrect : true},
+            {answerText : "Mount Everest", isCorrect : true},
             {answerText : "Mexico", isCorrect : false},
             {answerText : "Germany", isCorrect : false},
         ],
     },
-   
+    {
+        questionText : "Which country has the largest population in the world? ",
+        answerOption : [
+            {answerText : "China", isCorrect : true},
+            {answerText : "France", isCorrect : false},
+            {answerText : "Mexico", isCorrect : false},
+            {answerText : "Germany", isCorrect : false},
+        ],
+    },
+   //Easy questions
+   /**
+    * 
+    * 
+
+    Trivia Question: What are the names of the seven continents of the world?
+
+    Answer: Asia, Africa, North America, South America, Antarctica, Europe, Australia
+
+    Trivia Question: What are the names of the five oceans of the world?
+
+    Answer: Atlantic, Pacific, Indian, Arctic, and the Antarctic
+
+    Trivia Question: What is the name of the longest river in Africa?
+
+    Answer: The Nile River
+
+    Trivia Question: What American city is the Golden Gate Bridge located in?
+
+    Answer: San Francisco
+
+    Trivia Question: What is the capital of Mexico?
+
+    Answer: Mexico City
+
+    Trivia Question: What is the name of the largest country in the world?
+
+    Answer: Russia
+
+    Trivia Question: What U.S. state is home to no documented poisonous snakes?
+
+    Answer: Alaska
+
+    Related: Trivia Questions
+
+    Trivia Question: Where is the Eiffel Tower located?
+
+    Answer: Paris, France
+
+    Trivia Question: What is the capital of Canada?
+
+    Answer: Ottawa
+
+    Trivia Question: What is the name of the largest ocean in the world?
+
+    Answer: The Pacific Ocean
+
+    Trivia Question: What present-day Italian city does Mt. Vesuvius overlook?
+
+    Answer: Naples
+
+    Trivia Question: What country are the Great Pyramids of Giza located in?
+
+    Answer: Egypt
+
+    Trivia Question: What is the capital of Thailand?
+
+    Answer: Bangkok
+
+    Trivia Question: What is the name of the smallest country in the world?
+
+    Answer: The Vatican City
+
+    Trivia Question: What is the capital of the American State of California?
+
+    Answer: Sacramento
+
+    Trivia Question: What country has the most natural lakes?
+
+    Answer: Canada
+
+    Trivia Question: How many States does the United States consist of?
+
+    Answer: 50
+
+    Trivia Question: What planet is closest to Earth?
+
+    Answer: Venus
+
+    Trivia Question: What is the name of the river that flows through the Brazil rainforest?
+
+    Answer: The Amazon
+
+    Trivia Question: Which country is also called The Netherlands?
+
+    Answer: Holland
+
+    Trivia Question: What is the coldest place on Earth?
+
+    Answer: Antarctica
+
+    Trivia Question: What is the official currency of the United Kingdom?
+
+    Answer: Pound sterling
+
+    Trivia Question: What country lies above Mexico?
+
+    Answer: The United States of America
+    */
+   //Hard questions
 ];
 
-export default football_questions;
+export default geography_questions;
